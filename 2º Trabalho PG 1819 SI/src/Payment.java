383dd3b859157799e6f584522440ec26ff2722c1
Carneiro <ASUS@Mammoth.lan>
2018-11-11 14:41:41 +0000

    Class Payment updated
    Class Greeting changed to Greeting2

--- 2º Trabalho PG 1819 SI/src/Payment.java
+++ 2º Trabalho PG 1819 SI/src/Payment.java
@@ -4,46 +4,56 @@ public class Payment {
         Scanner kbd = new Scanner (System.in);
         int k=0;
         int num=0;
-        while( k < 1) {
+        while(k < 1) {
             num *= 10;
             num += args[0].charAt(k++) - '0';
         }
+        
         int [] coinsLeft = new int [8];
-        for(int i=0;i<coinsLeft.length;++i){
+        for(int i=0;i<coinsLeft.length;++i)
             coinsLeft[i]=num;
-        }
         int [] coinsUsed = {0,0,0,0,0,0,0,0};
-        double [] coinValue = {2,1,0.50,0.20,0.10,0.05,0.02,0.01};
+        final double [] COIN_VALUE = {2,1,0.50,0.20,0.10,0.05,0.02,0.01};
         double price, received, change;
         boolean noChange = false;
         String receipt = "";
         String euro = "\u20ac";
+         
         System.out.println("Maquina carregada com "+num+" moedas de cada tipo.");
         while(true){
+        	
             System.out.print("Preco (em euros)? ");
             price=kbd.nextDouble()*100;
-            System.out.print(price);
             if(price==0)
                 break;
+            int priceInt = (int)price;
+        	double priceDecimal = price-(int)price;
+            if(priceDecimal>=0.5)
+            	priceInt+=1;
+            
             System.out.print("Quantia recebida (em euros)? ");
-            received=kbd.nextDouble()*100;;
-            System.out.print(received);
-            change=(int)received-(int)price;
+            received=kbd.nextDouble()*100;
+            int receivedInt = (int)received;
+        	double receivedDecimal = received-(int)received;
+            if(receivedDecimal>=0.5)
+            	receivedInt+=1;
+            
+            change=receivedInt-priceInt;
             if(change==0)
                 System.out.println("Quantia certa");
             else if(change<0)
                 System.out.println("Quantia insuficiente");
             else{
                 receipt = "Demasia ("+(change/100)+") = ";
-                while(change!=0) {
-                    for (int i = 0; i < coinValue.length; i++) {
-                        while (change >= (coinValue[i] * 100)) {
+                while(change!=0 && !noChange) {
+                    for (int i = 0; i < COIN_VALUE.length; i++) {
+                        while (change >= (COIN_VALUE[i] * 100)) {
                             if (coinsLeft[i] != 0) {
-                                change -= (coinValue[i] * 100);
+                                change -= (COIN_VALUE[i] * 100);
                                 coinsUsed[i]++;
                                 coinsLeft[i]--;
                             }
-                            else if (i < coinValue.length) {
+                            else if (i < COIN_VALUE.length) {
                                 i++;
                                 if(i==8){
                                     noChange=true;
@@ -58,17 +68,18 @@ public class Payment {
                 if(!noChange) {
                     for (int i = 0; i < coinsUsed.length; i++)
                         if (coinsUsed[i] > 0)
-                            receipt = receipt + coinsUsed[i] + " x " + coinValue[i] + euro + " + ";
+                            receipt = receipt + coinsUsed[i] + " x " + COIN_VALUE[i] + euro + " + ";
                     receipt = receipt.substring(0, receipt.length() - 3) + ".";
                     System.out.print(receipt);
                 }
-                else
+                else {
                     System.out.print(receipt+" Nao ha troco. Venda interrompida.");
+                    noChange=false;
+                }
                 System.out.println();
 
                 for(int i=0;i<coinsUsed.length;i++)
                     coinsUsed[i] = 0;
-                noChange=false;
             }
         }
     }
